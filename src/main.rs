0238bf4cc50edfd21084db72a59137d339a85935
Zimo Li <7163127+lzm0@users.noreply.github.com>
2022-12-07 00:08:13 -0500

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,14 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
-use base64::decode;
 use serde::{Deserialize, Serialize};
-use std::io::prelude::*;
+use std::io::{prelude::*, Error, ErrorKind};
 use std::process::{Command, Stdio};
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::post().to(handle_post)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
-}
 
 #[derive(Deserialize, Serialize)]
 struct AudioData {
     data: String,
 }
 
-async fn handle_post(body: web::Json<AudioData>) -> HttpResponse {
-    let input_data = decode(&body.data).unwrap();
-    let output_data = convert(input_data);
-    let output_str = base64::encode(&output_data);
-    HttpResponse::Ok().json(AudioData { data: output_str })
-}
-
-fn convert(input_data: Vec<u8>) -> Vec<u8> {
+fn convert(input_data: Vec<u8>) -> Result<Vec<u8>, Error> {
     let mut cmd = Command::new("ffmpeg")
         .arg("-i")
         .arg("-")
@@ -35,20 +19,44 @@ fn convert(input_data: Vec<u8>) -> Vec<u8> {
         .arg("-")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+        .spawn()?;
 
-    cmd.stdin.as_mut().unwrap().write_all(&input_data).unwrap();
+    let stdin = cmd
+        .stdin
+        .as_mut()
+        .ok_or_else(|| Error::new(ErrorKind::Other, "Failed to open stdin for ffmpeg"))?;
 
-    let output = cmd.wait().unwrap();
-    assert!(output.success());
+    stdin.write_all(&input_data)?;
+
+    let output = cmd.wait_with_output()?;
+
+    if !output.status.success() {
+        return Err(Error::new(ErrorKind::Other, "ffmpeg failed"));
+    }
 
     let mut output_data = Vec::new();
-    cmd.stdout
-        .as_mut()
-        .unwrap()
-        .read_to_end(&mut output_data)
-        .unwrap();
+    output.stdout.take(1024).read_to_end(&mut output_data)?;
 
-    output_data
+    Ok(output_data)
+}
+
+async fn handle_post(body: web::Json<AudioData>) -> HttpResponse {
+    let input_data = match base64::decode(&body.data) {
+        Ok(data) => data,
+        Err(_) => return HttpResponse::BadRequest().body("Payload is not base64 encoded"),
+    };
+    let output_data = match convert(input_data) {
+        Ok(data) => data,
+        Err(err) => return HttpResponse::InternalServerError().body(err.to_string()),
+    };
+    let output_str = base64::encode(&output_data);
+    HttpResponse::Ok().json(AudioData { data: output_str })
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::post().to(handle_post)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
